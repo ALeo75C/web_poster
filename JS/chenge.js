a26db915c50f7785efb639e1bfa1c217474b76f9
ALeo75C <za.iz.pr@yandex.kz>
2018-12-20 01:47:45 +0300

    decktop

---
+++ JS/chenge.js
@@ -0,0 +1,41 @@
+$('.wint').click(function () {
+        $('body').css('background-color', '#F5F7FB');
+        $('.scroll').css('background-color', '#F5F7FB');
+        $('.scroll>.card').css('background-color', '#D2DCFB');
+        $('.horizontal>.card').css('background-color', '#D2DCFB');
+        $('.backUp').attr('src', 'images/Up_WINT.svg');
+        $('.backEnd').attr('src', 'images/Down_WINT.svg');
+        $('.snow').svg('fill', '#7F9AEF');
+    });
+$('.sum').click(function () {
+        $('body').css('background-color', '#E0FF80'),('transition', '20s');
+        $('.scroll').css('background-color', '#E0FF80');
+        $('.scroll>.card').css('background-color', '#B4ED06');
+        $('.horizontal>.card').css('background-color', '#B4ED06');
+        $('.backUp').attr('src', 'images/Up_SUM.svg');
+        $('.backEnd').attr('src', 'images/Down_SUM.svg');
+    });
+$('.out').click(function () {
+        $('body').css('background-color', '#FCF6B1');
+        $('.scroll').css('background-color', '#FCF6B1');
+        $('.scroll>.card').css('background-color', '#FFCF70');
+        $('.horizontal>.card').css('background-color', '#FFCF70');
+        $('.backUp').attr('src', 'images/Up_OUT.svg');
+        $('.backEnd').attr('src', 'images/Down_OUT.svg');
+    });
+$('.sun').click(function () {
+        $('body').css('background-color', '#FFF997');
+        $('.scroll').css('background-color', '#FFF997');
+        $('.scroll>.card').css('background-color', '#FFF446');
+        $('.horizontal>.card').css('background-color', '#FFF446');
+        $('.backUp').attr('src', 'images/Up_SUN.svg');
+        $('.backEnd').attr('src', 'images/Down_SUN.svg');
+    });
+$('.win').click(function () {
+        $('body').css('background-color', '#89EAED');
+        $('.scroll').css('background-color', '#89EAED');
+        $('.scroll>.card').css('background-color', '#FFE5C0');
+        $('.horizontal>.card').css('background-color', '#FFE5C0');
+        $('.backUp').attr('src', 'images/Up_WIN.svg');
+        $('.backEnd').attr('src', 'images/Down_WIN.svg');
+    });
